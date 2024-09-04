75ccc42be235789ee4434300815ac959f7903929
Andrew Morton <andrew.morton@mongodb.com>
2024-09-04 08:16:27 +0000

    Move WT_MYSLOT into log_private.h

--- src/log/log_private.h
+++ src/log/log_private.h
@@ -1,5 +1,19 @@
 #pragma once
 
+struct __wt_myslot {
+    WT_LOGSLOT *slot;    /* Slot I'm using */
+    wt_off_t end_offset; /* My end offset in buffer */
+    wt_off_t offset;     /* Slot buffer offset */
+
+/* AUTOMATIC FLAG VALUE GENERATION START 0 */
+#define WT_MYSLOT_CLOSE 0x1u         /* This thread is closing the slot */
+#define WT_MYSLOT_NEEDS_RELEASE 0x2u /* This thread is releasing the slot */
+#define WT_MYSLOT_UNBUFFERED 0x4u    /* Write directly */
+                                     /* AUTOMATIC FLAG VALUE GENERATION STOP 32 */
+    uint32_t flags;
+};
+typedef struct __wt_myslot WT_MYSLOT;
+
 /* DO NOT EDIT: automatically built by prototypes.py: BEGIN */
 
 extern int __wti_log_acquire(WT_SESSION_IMPL *session, uint64_t recsize, WT_LOGSLOT *slot)
